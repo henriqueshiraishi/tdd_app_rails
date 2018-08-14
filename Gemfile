b9a30cd0b930b01f94ba3038b3df41ce845b60f6
Henrique Shiraishi <henriqueashiraishi@gmail.com>
2018-08-14 16:16:58 -0300

    Testando o Link de cadastro e Cadastro de Cliente

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ end
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.6'
+gem 'rails-i18n', '~> 5.1'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
@@ -38,6 +39,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '~> 3.7'
   gem 'capybara'
+  gem 'faker'
 end
 
 group :development do
---
+++ spec/features/customers_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.feature "Customers", type: :feature do
+  context 'Redirecionamentos' do
+    scenario 'Verifica Link Cadastro de Clientes' do
+      visit(root_path)
+      expect(page).to have_link('Cadastro de Clientes')
+    end
+
+    scenario 'Verifica Link de Novo Cliente' do
+      visit(root_path)
+      click_on('Cadastro de Clientes')
+      expect(page).to have_content('Listando Clientes')
+      expect(page).to have_link('Novo Cliente')
+    end
+
+    scenario 'Verifica Formulário de Novo Cliente' do
+      visit(customers_path)
+      click_on('Novo Cliente')
+      expect(page).to have_content('Novo Cliente')
+    end
+  end
+
+  context 'Ações dos métodos' do
+    scenario 'Cadastra um cliente válido' do
+      visit(new_customer_path)
+      customer_name = Faker::Name.name
+      fill_in('customer_name',with: customer_name)
+      fill_in('customer_email',with: Faker::Internet.email)
+      fill_in('customer_phone',with: Faker::PhoneNumber.phone_number)
+      attach_file('customer_avatar', "#{Rails.root}/spec/fixtures/avatar.png")
+      choose(option: ['S','N'].sample)
+      click_on('Criar Cliente')
+      expect(page).to have_content('Cliente cadastrado com sucesso.')
+      expect(Customer.last.name).to eq customer_name
+    end
+  end
+
+end
